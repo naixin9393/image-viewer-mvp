6c356a4163b1c7fa3aa7a41c5f3dc2506c4ae564
naixin9393 <naicinchen@gmail.com>
2023-12-21 17:03:34 +0000

    feat(gui): show solid color

--- src/main/java/com/imageviewer/view/fx/FXImageDisplay.java
+++ src/main/java/com/imageviewer/view/fx/FXImageDisplay.java
@@ -1,7 +1,12 @@
 package com.imageviewer.view.fx;
 
 import com.imageviewer.model.ImageDisplay;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+
+import static javafx.scene.paint.Color.RED;
 
 public class FXImageDisplay extends ImageView implements ImageDisplay {
     @Override
@@ -15,7 +20,18 @@ public class FXImageDisplay extends ImageView implements ImageDisplay {
 
     @Override
     public void paint(String image, int offset) {
+        WritableImage writableImage = new WritableImage(500, 500);
+        fillWith(writableImage, image);
+        this.setImage(writableImage);
+    }
 
+    private static void fillWith(WritableImage writableImage, String color) {
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+        for (int i = 0; i < 500; i++) {
+            for (int j = 0; j < 500; j++) {
+                pixelWriter.setColor(i, j, RED);
+            }
+        }
     }
 
     @Override
